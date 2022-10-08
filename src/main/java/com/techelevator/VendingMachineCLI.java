799775344bbe7d7ae2c54cbcc6547ccfd59f08cf
Jeff Justo <jeff.justo@gmail.com>
2022-10-07 17:05:38 -0700

    Completed menu options and log.txt

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -4,13 +4,16 @@ import com.techelevator.view.Menu;
 import com.techelevator.view.Purchase;
 import com.techelevator.view.Snacks;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class VendingMachineCLI {
 
@@ -23,14 +26,18 @@ public class VendingMachineCLI {
 
 	private static final String PURCHASE_MENU_FEED_MONEY = "Feed Money";
 	private static final String PURCHASE_MENU_SELECT_ITEM = "Select Item";
-	private static final String PURCHASE_MENU_EXIT = "Exit to Main Menu";
+	private static final String PURCHASE_MENU_EXIT = "Finish Transaction";
 	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_FEED_MONEY,
 			PURCHASE_MENU_SELECT_ITEM,
 			PURCHASE_MENU_EXIT};
 
+	DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
+	LocalDateTime timeNow = LocalDateTime.now();
+	String dateLog = timeNow.format(dateTime);
+
 	private Menu menu;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
@@ -40,85 +47,104 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
-	public void run() {
+	public void run() throws IOException {
 		Inventory updatedInventory = new Inventory();
 		Inventory stock = new Inventory();
 		List<Snacks> stockDisplay = stock.stockInventory();
+		Purchase itemPurchase = new Purchase();
 
-		boolean vendingLoop = true;
-		while (vendingLoop) {
-			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
-			System.out.println();
-
-			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				//METHOD TO DISPLAY MENU/INVENTORY
-				for (Snacks item : stockDisplay) {
-					System.out.printf("%-2s | %-20s | %-4s | %2s \n", item.getSlot() ,item.getName(), "$" +item.getPrice(), item.getInventory());
-				}
+		File logFile = new File("log.txt");
+		logFile.createNewFile();
+		if (logFile.exists()) {
+			PrintWriter purchaseLog = new PrintWriter(new FileWriter(logFile, false));
 
 
 
-			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				boolean purchaseLoop = true;
-				while (purchaseLoop) {
-					String purchaseChoice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+			boolean vendingLoop = true;
+			while (vendingLoop) {
+				String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+				System.out.println();
 
-					if (purchaseChoice.equals(PURCHASE_MENU_FEED_MONEY)) {
-						//METHOD TO FEED MONEY
-						System.out.println("Select dollar amount $1, $5, $10, $20:");
-						Scanner userInput = new Scanner(System.in);
-						String dollarChoice = userInput.nextLine();
-						balance = balance.add(new BigDecimal(dollarChoice));
-						System.out.println("Your balance is: " + "$" + balance);
+				if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+					//METHOD TO DISPLAY MENU/INVENTORY
+					for (Snacks item : stockDisplay) {
+						System.out.printf("%-2s | %-20s | %-4s | %2s \n", item.getSlot(), item.getName(), "$" + item.getPrice(), item.getInventory());
+					}
 
 
+				} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+					boolean purchaseLoop = true;
+					while (purchaseLoop) {
+						String purchaseChoice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+
+						if (purchaseChoice.equals(PURCHASE_MENU_FEED_MONEY)) {
+							//METHOD TO FEED MONEY
+							System.out.println("Select dollar amount $1, $5, $10, $20:");
+							Scanner userInput = new Scanner(System.in);
+							String dollarChoice = userInput.nextLine();
+							balance = balance.add(new BigDecimal(dollarChoice));
+							System.out.println("Your balance is: " + "$" + balance);
+							purchaseLog.println(dateLog + " FEED MONEY " + "$" + dollarChoice + " " + balance);
+							purchaseLog.flush();
 
 
 
-					} else if (purchaseChoice.equals(PURCHASE_MENU_SELECT_ITEM)) {
-						//METHOD FOR MENU SELECTION
-						for (Snacks item : stockDisplay) {
-							System.out.printf("%-2s | %-20s | %-4s | %2s \n", item.getSlot(), item.getName(), "$" + item.getPrice(), (item.getInventory()));
-						}
-
-						System.out.println("Select an item from the menu >>>");
-						Scanner userInput = new Scanner(System.in);
-						String selection = userInput.nextLine();
-						Purchase itemPurchase = new Purchase();
-						BigDecimal zero = new BigDecimal("0");
-
-
-						if(itemPurchase.updatedBalance(balance, selection).compareTo(zero) >= 0) {
-							BigDecimal newBalance = itemPurchase.updatedBalance(balance, selection);
-
-							balance = newBalance;
-
-							System.out.println("Your new balance is: $" + balance);
+						} else if (purchaseChoice.equals(PURCHASE_MENU_SELECT_ITEM)) {
+							//METHOD FOR MENU SELECTION
 							for (Snacks item : stockDisplay) {
-								if (item.getSlot().equals(selection))
-									System.out.println(item.getNoise());
-
+								System.out.printf("%-2s | %-20s | %-4s | %2s \n", item.getSlot(), item.getName(), "$" + item.getPrice(), (item.getInventory()));
 							}
-							for (Snacks item : stockDisplay) {
-								if (item.getSlot().equals(selection) ) {
-									item.setInventory(item.getInventory() - 1);
-										if(item.getInventory() < 0){
+
+							System.out.println("Select an item from the menu >>>");
+							Scanner userInput = new Scanner(System.in);
+							String selection = userInput.nextLine();
+							BigDecimal zero = new BigDecimal("0");
+
+
+							if (itemPurchase.updatedBalance(balance, selection).compareTo(zero) >= 0) {
+								BigDecimal newBalance = itemPurchase.updatedBalance(balance, selection);
+
+								balance = newBalance;
+
+								System.out.println("Your new balance is: $" + balance);
+								for (Snacks item : stockDisplay) {
+									if (item.getSlot().equals(selection)) {
+										System.out.println(item.getNoise());
+
+
+								}
+								purchaseLog.println(dateLog + item.getName() + " " + item.getSlot() + "$" + item.getPrice() + " " + balance);
+								purchaseLog.flush();
+								break;
+							}
+								for (Snacks item : stockDisplay) {
+									if (item.getSlot().equals(selection)) {
+										item.setInventory(item.getInventory() - 1);
+										if (item.getInventory() < 0) {
 											System.err.println("Item is SOLD OUT");
 										}
+									}
 								}
+
+							} else if (itemPurchase.updatedBalance(balance, selection).compareTo(zero) < 0) {
+								System.err.println("Not enough money. Please feed more money.");
 							}
 
-						} else if (itemPurchase.updatedBalance(balance, selection).compareTo(zero) < 0 ){
-							System.err.println("Not enough money. Please feed more money.");
-						}
+						} else if (purchaseChoice.equals(PURCHASE_MENU_EXIT)) {
+							itemPurchase.getChange(balance);
+							purchaseLog.println(dateLog + " GIVE CHANGE " + balance + "0.00");
+							purchaseLog.flush();
+							purchaseLog.close();
 
-					} else if(purchaseChoice.equals(PURCHASE_MENU_EXIT)) {
-						purchaseLoop = false;
+
+							purchaseLoop = false;
+						}
 					}
 				}
 			}
-		}
 
+
+		}
 	}
 }
 
--- src/main/java/com/techelevator/view/Purchase.java
+++ src/main/java/com/techelevator/view/Purchase.java
@@ -2,11 +2,25 @@ package com.techelevator.view;
 
 import com.techelevator.Inventory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Purchase {
+
+    LocalDateTime dateTime = LocalDateTime.now();
+
+    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+
+    private BigDecimal balance = new BigDecimal(0.00);
 
     // method in Purchase
     public BigDecimal updatedBalance(BigDecimal balance, String input) {
@@ -21,4 +35,23 @@ public class Purchase {
         return balance;
 
     }
+
+    public void getChange( BigDecimal balance){
+        balance = balance.multiply(new BigDecimal("100"));
+        int coinBalance = balance.intValue();
+        int numOfquarters;
+        int numOfdimes;
+        int numOfnickels;
+
+        numOfquarters = coinBalance / 25;
+        coinBalance = coinBalance % 25;
+        numOfdimes = coinBalance / 10;
+        coinBalance = coinBalance % 10;
+        numOfnickels = coinBalance / 5;
+        System.out.println("Your change is " + numOfquarters + " quarters, " + numOfdimes + " dimes, " + numOfnickels + " nickels");
+    }
+
+
+
+
 }
